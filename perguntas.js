346f93f04b596eb6f3541860b463cd1e7b3c0299
Gustavo Alves <140844146+KiriguaKirto1@users.noreply.github.com>
2024-09-24 15:52:21 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,41 @@
+criaCartao(
+    'Programação',
+    'Qual a linguagem de programação utilizada nessa página',
+    'Utilizamos HTML e CSS'
+)
+
+criaCartao(
+    'Geografia',
+    'O Brasil fica em qual continente?',
+    'O Brasil fica localizado na América do Sul'
+)
+
+criaCartao(
+    'Ciência',
+    'Qual a fórmula química da água?',
+    'A formula química é H2O'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se fala Página em inglês?',
+    'Página em Inglês é Page'
+)
+
+criaCartao(
+    'Matemática',
+    'Como se fala Página em inglês?',
+    'Página em Inglês é Page'
+)
+
+criaCartao(
+    'História',
+    'Em que século o Brasil foi descoberto?',
+    'Foi descoberto no século XV (15)'
+)
+
+
+
+
+
+
